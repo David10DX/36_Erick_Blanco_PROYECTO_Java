a6e33c13ebf348a60bcb71b8e7eb2216aec5ab2f
David10DX <79998512+David10DX@users.noreply.github.com>
2021-10-14 17:15:41 -0600

    primer commit

---
+++ src/principal.java
@@ -0,0 +1,9 @@
+public class principal {
+    public static void main(String[] args) {
+        widget v1 = new widget();
+        v1.setVisible(true);
+
+        //formu v2 = new formu();
+        //v2.setVisible(true);
+    }
+}
